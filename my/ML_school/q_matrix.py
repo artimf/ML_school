74b92c2af7e760d582e954fb85b75c6ab618af6b
artimf <artimf@gmail.com>
2019-11-06 17:20:55 +0700

    auto commit

--- my/ML_school/q_matrix.py
+++ my/ML_school/q_matrix.py
@@ -1,79 +1,128 @@
+# -*- coding: utf-8 -*-
 """
-В этом задание мы спрогнозируем норму сбережения M2-M0 байесовским методом, используя вероятностный язык программирования Stan. В задании нужно будет смоделировать сезонную компоненту.
+Created on Wed Nov  6 15:50:46 2019
 
-Stan - высокопроизводительный фреймворк для байесовских моделей и не только. На нем удобно сформулировать модель в виде уравнений, и далее программа методом точечной оптимизации, MCMC, или Variational Inference оценивает параметры с высокой производительностью. От конкурентов его отличает очень хорошая реализация алгоритма HMC (Hamilton Markov Chain). Подробнее о Stan - https://mc-stan.org/users/documentation/.
+@author: 16690918
 
-Stan manual - https://github.com/stan-dev/stan/releases/download/v2.17.1/stan-reference-2.17.1.pdf
+Q_MATRIX
 """
+
+
+#%%
 
 import pandas as pd
 import numpy as np
-import warnings
-warnings.filterwarnings('ignore')
+# Коэффициент дисконтирования будущих наград
+gamma = 0.95
+# Параметр скорости обучения
+learning_rate = 0.5
+# Критерий сходимости алгоритма
+tol = 1e-3
+experience_buffer = pd.read_csv('Grid_world.csv', dtype = int)
+print(experience_buffer[:10])
 
-print("1")
-data = pd.read_csv(r"data_saving_rate.csv",sep=';',index_col='Date', decimal=',')
-data.index = pd.to_datetime(data.index, format='%d.%m.%Y')
-data = data.dropna(axis=1)#удалить null ось 1
-print(data.head())
+#%%
+#Определение множеств возможных состояний и допустимых действий
+# Множество возможных состояний
+state_space = sorted(list(set(experience_buffer['s'])))
+print('Множество возможных состояний',state_space)
 
-data['M_growth'] = data['M2-M0'][1:] - data.shift(1)['M2-M0'][1:] # прирост M2-M0 будет нашей целевой переменной
-data = data.dropna(axis=0)
-print(data.head())
+# Множество допустимых действий
+action_space = sorted(list(set(experience_buffer['a'])))
+print('Множество допустимых действий',action_space)
 
-#Создадим файл с кодом на языке stan. Допишите кусок кода. '//' - комментарии в Stan
-#скомпилируем код stan в машинный код
-#python -m pip install pystan --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org
-#https://pystan.readthedocs.io/en/latest/windows.html
-#conda config --set ssl_verify false 
-#conda install libpython m2w64-toolchain -c msys2
-#conda install pystan 
-import pystan
-model = pystan.StanModel(file='model.stan')
 
-#разобьем данные на тестовую и обучающую выборку
-factor = data.M_growth
-y = data.Savings_rate
+# Множество допустимых наград
+action_space = sorted(list(set(experience_buffer['r'])))
+print('Множество допустимых наград',action_space)
+
+#####################################################################
+#Создание Q-матрицы
+#################### Начало оцениваемого задания ####################
+#####################################################################
+
+# Создайте Q-матрицу, в которой строки соответствуют возможным состояниям, а столбцы - допустимым действиям
+#Q_matrix = pd.DataFrame(0.0, index=None, columns=None) 
+import numpy as np
+nda1 = np.zeros(400).reshape(100,4)  
+#nda1 = np.arange(400).reshape(100,4) 
+Q_matrix = pd.DataFrame(nda1,index=np.arange(1, 101, 1),columns=[1,2,3,4])  
+#####################################################################
+#################### Конец оцениваемого задания #####################
+#####################################################################
+# Копия Q-матрицы на предыдущей итерации для проверки сходимости алгоритма
+Q_old = Q_matrix.copy()  
+
+#%%
  
-length = len(y)
-factor_study = factor.head(int(2 / 3 * length))
-factor_test = factor.tail(length-int(2 / 3 * length))
 
-y_study = y.head(int(2 / 3 * length))
-y_test = y.tail(length-int(2 / 3 * length))
+continue_condition = True
+i = 0
+print(Q_matrix[10:23])
+# Итерационный цикл
+while (continue_condition):
 
+    # Счетчик количества итераций
+    i += 1
 
-#подготовим данные для stan
-N = len(y_study)
-P = len(y_test)
-D = 1
-y_stan = np.array(y_study)
-x_stan = [np.array(factor)]
-data_stan = dict(N=N, D=D, P=P, y=y_stan, x=x_stan)
+    # Последовательно обрабатываем каждое наблюдение в experience buffer
+    for index, experience in experience_buffer.iterrows():
 
-#обучим модель и сделаем прогноз
-#Мы применяем метод Markov Chain Monte Carlo для симуляций из постериорного распределения параметров.
-#В данном случае мы делаем 2000 симуляций и 2 попытки.
-#В модели могут всплыть предупреждения о сходимости и т.п. Так как в алгоритме MCMC много параметров управления,
-#которые следует вдумчиво подобирать. Здесь мы просто знакомимся с методом. 
-#fitModel = model.sampling(data=data_stan, iter=200, chains=2)
-fitModel = model.sampling(data=data_stan, iter=1, chains=1)
+        # Текущее состояние
+        s = experience["s"]
 
-#Построим графики
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-plt.plot(y, c='green')  # факт
-yfittedAndForecasted = np.mean( np.array(fitModel.extract()['predY']), axis = 0) #fitted_value + прогноз
-yforecasted = yfittedAndForecasted[-len(y_test):]# прогноз
-plt.plot(y_test.index, yforecasted, c='red')
-plt.show()
+        # Выбранное действие
+        a = experience["a"]
 
-stanModRmse = np.sqrt(np.mean((yforecasted - y[-len(y_test):])**2))
-stanModRmse
+        # Полученная награда
+        r = experience["r"]
 
-naiveModelRmse = np.sqrt(np.mean(( np.mean(y_test) - y[-len(y_test):] )**2)) # модель, в которой прогноз целевой переменной равен среднему значению предыдущих.
-naiveModelRmse
+        # Следующее состояние
+        s_next = experience["s'"]
 
-#Проверка!
-stanModRmse < naiveModelRmse
- 
+        #####################################################################
+        #################### Начало оцениваемого задания ####################
+        #####################################################################
+
+        # Вычислите значение Q-матрицы при совершении действия a в состоянии s. Используйте метод loc.
+        #Q_s_a = None
+        Q_s_a = Q_matrix.loc[s,a] 
+        #print('>',s,a,Q_s_a,'>',s_next) 
+
+        # Вычислите максимальное значение Q-матрицы в состоянии s_next. Используйте методы loc и max.
+        #Q_next_max = None
+        Q_next_max = max(Q_matrix.loc[s_next])
+        #print('>>>>',s_next,'>',Q_next_max) 
+
+        # Запишите уравнение обновления Q-матрицы. Не забудьте учесть параметр скорости обучения(learning_rate)
+        #Q_matrix.loc[s,a] = None 
+        Q_matrix.loc[s,a]=(r+Q_s_a+Q_next_max*gamma)*learning_rate
+        #print(Q_matrix[10:23])
+        #break
+
+        #####################################################################
+        #################### Конец оцениваемого задания #####################
+        #####################################################################
+    #break    
+    # Вычисление коэффициента сходимости. После окончания первой итерации он должен быть равен 16009.8365. 
+    convergence_rate = (Q_matrix - Q_old).abs().sum().sum()
+    print ('Итерация %d завершена. Коэффициент сходимости: %.4f ' % (i, convergence_rate))
+    # Проверка выполнения условия сходимости
+    if (convergence_rate < tol):
+        continue_condition = False
+    else:
+        Q_old = Q_matrix.copy()
+
+# Завершение алгоритма. Алгоритм должен завершиться после 8 итераций.
+print ('\nНеобходимый коэффициент сходимости был достигнут.')
+print(Q_matrix)
+#%%
+# Для каких состояний вывести оптимальные действия?
+output_states = [41,42,43,44,45,46,47,48,49,50]
+#output_states = np.arange(1, 101, 1)
+
+# Вывод оптимальных действий
+optimal_strategy = Q_matrix.idxmax(axis=1).loc[output_states].reset_index()
+#print(tabulate(optimal_strategy.values, headers = ['Состояние', 'Оптимальное действие']))
+print(optimal_strategy.values)
+#%%
