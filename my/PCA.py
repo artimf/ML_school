91c776c5205c6de9096912a306216858b8e141e6
artimf <artimf@gmail.com>
2019-11-14 18:39:07 +0700

    auto commit

--- my/PCA.py
+++ my/PCA.py
@@ -20,7 +20,6 @@ X= data[[u'fixed acidity',u'volatile acidity',u'citric acid',u'residual sugar',u
 y=data.quality
 X= preprocessing.StandardScaler().fit(X).transform(X)
 #%%
-print('print(y)',y)
 
 model= PCA()
 results = model.fit(X)
@@ -28,8 +27,40 @@ Z=results.transform(X)
 plt.plot(results.explained_variance_)
 plt.show()
 
-pd.DataFrame(results.components_, columns=list(
+#%%
+hidenVar=pd.DataFrame(results.components_, columns=list(
         [u'fixed acidity', u'volatile acidity', u'citric acid', u'residual sugar', 
          u'chlorides', u'free sulfur dioxide', u'total sulfur dioxide', u'density',
          u'pH', u'sulphates', u'alcohol']))
 
+print(hidenVar)
+print(list(hidenVar))
+
+#%%
+#прогнозирование качества вина до применнеия анализа главных компонент
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+gnb = GaussianNB()
+fit = gnb.fit(X,y)
+pred=fit.predict(X)
+print(confusion_matrix(pred,y)) 
+print('сумма всех предсказаний ',confusion_matrix(pred,y).sum())#сумма всех предсказаний
+print('количесвто верных предсказаний ',confusion_matrix(pred,y).trace())#количесвто верных предсказаний
+print(accuracy_score(pred, y))
+#%%
+#%%
+#прогнозирование качества вина с наращиванием количества гланых компонент
+predicted_correct =[]
+for i in range(1,10):
+    model=PCA(n_components=i)
+    results=model.fit(X)
+    Z=results.transform(X)
+    fit=gnb.fit(Z,y)
+    pred=fit.predict(Z)
+    predicted_correct.append(confusion_matrix(pred,y).trace())
+    print(predicted_correct)
+plt.plot(predicted_correct)
+plt.show()
+#%%
